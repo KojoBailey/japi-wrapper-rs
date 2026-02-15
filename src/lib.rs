d9ad02edf4149b18e72e1d1e498cfaa0edf73217
Kojo Bailey <kojomolojo@gmail.com>
2026-02-15 16:52:56 +0000

    Add log()

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,17 @@ use std::ffi::CString;
 use std::os::raw::c_char;
 use std::sync::OnceLock;
 
+#[repr(i32)] // Matches C default.
+#[derive(Debug, Clone, Copy)]
+pub enum LogLevel {
+    Fatal = 0,
+    Error = 1,
+    Warn  = 2,
+    Info  = 3,
+    Debug = 4,
+    Trace = 5,
+}
+
 static API: OnceLock<JoJoAPI> = OnceLock::new();
 
 pub struct JoJoAPI {
@@ -15,6 +26,16 @@ impl JoJoAPI {
             let lib = Library::new(dll_path)?;
             Ok(JoJoAPI { lib })    
         }
+    }
+
+    pub fn log(&self, level: LogLevel, msg: &str) -> Result<(), Box<dyn std::error:Error>> {
+        let c_str = CString::new(msg)?;
+        unsafe {
+            let func: Symbol<extern "C" fn(i32, *const c_char)> =
+                self.lib.get(b"JAPI_LogMessage")?;
+            func(level as i32, c_str.as_ptr());
+        }
+        Ok(())
     }
 }
 
