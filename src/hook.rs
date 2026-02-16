8a40811b8b194b71e597b1f9e44cbe911f9e70f4
Kojo Bailey <kojomolojo@gmail.com>
2026-02-16 15:51:41 +0000

    Bug fix

--- src/hook.rs
+++ src/hook.rs
@@ -13,7 +13,8 @@ struct JAPIHookMetaRaw {
     detour: *const c_void,
     original: *mut *const c_void,
     name: *const c_char,
-    is_game_function: u32,
+    is_game_function: bool,
+    _padding: [u8; 7],
 }
 
 pub fn register_hook<F>(
@@ -23,7 +24,7 @@ pub fn register_hook<F>(
     name: &str,
     is_game_function: bool,
 ) -> Option<u64> {
-    let c_name = CString::new(name).unwrap().into_raw();
+    let c_name = CString::new(name).expect("Failed to create CString.").into_raw();
 
     let handle = unsafe {
         JAPI_RegisterHook(JAPIHookMetaRaw{
@@ -31,7 +32,8 @@ pub fn register_hook<F>(
             detour: std::mem::transmute_copy::<F, *const c_void>(&detour),
             original: original.as_ptr() as *mut *const c_void,
             name: c_name,
-            is_game_function: is_game_function as u32,
+            is_game_function,
+            _padding: [0; 7],
         })
     };
 
