3b3cd08938284bc833ef3ae9e842c0fac30521a9
Kojo Bailey <kojomolojo@gmail.com>
2026-02-20 23:38:37 +0000

    Have base address return as *u8 instead of *u64, and add `convert_relative_module_address()`

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,10 @@ unsafe extern "C" {
     fn JAPI_GetModuleBaseAddress() -> *const u64;
 }
 
-pub fn get_module_base_address() -> *const u64 {
-    unsafe { JAPI_GetModuleBaseAddress() }
+pub fn get_module_base_address() -> *const u8 {
+    unsafe { JAPI_GetModuleBaseAddress() as *const u8 }
+}
+
+pub fn convert_relative_module_address(relative_address: usize) -> *const u8 {
+    unsafe { get_module_base_address().add(relative_address) }
 }
