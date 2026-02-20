029150dd49a16334a03783e1a139e65a74a40afd
Kojo Bailey <kojomolojo@gmail.com>
2026-02-20 22:01:15 +0000

    Add get_module_base_address()

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,12 @@ mod hook;
 pub use mod_meta::*;
 pub use log::*;
 pub use hook::*;
+
+#[link(name = "JAPI")]
+unsafe extern "C" {
+    fn JAPI_GetModuleBaseAddress() -> *const u64;
+}
+
+pub fn get_module_base_address() -> *const u64 {
+    unsafe { JAPI_GetModuleBaseAddress() }
+}
